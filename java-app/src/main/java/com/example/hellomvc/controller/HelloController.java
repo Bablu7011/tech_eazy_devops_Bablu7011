35b204256b43667b5392e2938421b2a11c62a1b9
Bablu7011 <babludevops7011@gmail.com>
2025-10-11 16:53:30 +0530

    pushed new changes

--- java-app/src/main/java/com/example/hellomvc/controller/HelloController.java
+++ java-app/src/main/java/com/example/hellomvc/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-        model.addAttribute("message", "Hello from Spring MVC! world");
+        model.addAttribute("message", "Hello from Spring MVC! hello new world");
         return "hello";
     }
 }
