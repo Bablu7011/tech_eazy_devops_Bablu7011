f0e91cecba8835ebd4d30f33a3791c130c79866f
Bablu7011 <babludevops7011@gmail.com>
2025-10-21 17:20:05 +0530

    new chnages

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "stage" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
